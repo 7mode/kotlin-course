98e330868c233e1e88e97943d1c7dc7a095648f9
7mode <muhamad.najjar@fiverr.com>
2020-03-22 12:55:53 +0200

    strings

--- Declarations/src/learn/kotlin/declarations/Declarations.kt
+++ Declarations/src/learn/kotlin/declarations/Declarations.kt
@@ -21,21 +21,41 @@ fun main(args: Array<String>) {
     println(employeeTwo !== employeeThree)
 
 
-//    var number: Int
-//    number = 10
-//    val number2 = 20
-//
-//    val employees: EmployeeSet
-//
-//    val employee1 = Employee("Muhamad", 1)
-//    employee1.name = "Muhamad Najjar"
-//
-//    val employee2: Employee
-//    if (number < number2) {
-//        employee2 = Employee("Nx", 2)
-//    } else {
-//        employee2 = Employee("Nx", 3)
-//    }
+    var number: Int
+    number = 10
+    val number2 = 20
+
+    val employees: EmployeeSet
+
+    val employee1 = Employee("Muhamad", 1)
+    employee1.name = "Muhamad Najjar"
+
+    val employee2: Employee
+    if (number < number2) {
+        employee2 = Employee("Nx", 2)
+    } else {
+        employee2 = Employee("Nx", 3)
+    }
+
+    println(employee1)
+
+    val change = 3.4
+    println("Your change is $$change")
+
+    val num1 = 234
+    val num2 = 654
+
+    println("The value of $num1 divided by $num2 is ${num1 / num2}")
+
+    println("The employee's id is ${employee1.id}")
+
+    val myName= "Muhamad"
+    val justLongString = """My name is $myName
+        |I'm learning strings in Kotlin!
+        |Wohow!!
+    """.trimMargin()
+
+    println(justLongString)
 }
 
 class Employee(var name: String, val id: Int) {
@@ -46,5 +66,9 @@ class Employee(var name: String, val id: Int) {
         }
 
         return false
+    }
+
+    override fun toString(): String {
+        return "Employee(name=$name, id=$id)"
     }
 }
