9035d24056a303348c68ce2e0cda64cca91abc51
7mode <muhamad.najjar@fiverr.com>
2020-03-22 13:26:05 +0200

    challenge 1

---
+++ Challenge1/src/academy/learningprogramming/challenge1/Challenge1.kt
@@ -0,0 +1,23 @@
+package academy.learningprogramming.challenge1
+
+fun main(args: Array<String>) {
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+    println("hello1 is referentially equal to hello2 ${hello1 === hello2}")
+    println("hello1 is structurally equal to hello2 ${hello1 == hello2}")
+
+    var num = 2988
+
+    val text: Any = "The sny type is the root of the Kotlin class hierarchy"
+
+    if (text is String) {
+        println(text.toUpperCase())
+    }
+
+    println("""   1
+        |  11
+        | 111
+        |1111  
+    """.trimMargin())
+}
